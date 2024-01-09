f0110f30e458bab5200b216e3d04fe1c53a20043
Mike Ton <mike.ton@gmail.com>
2024-01-09 06:48:56 -0600

    Uptodate with original progress before nuking repro lol

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
@@ -8,22 +10,26 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            files: Vec::new(),
-            number_lines: false,
-            number_nonblank_lines: false,
-        }
-    }
-}
-
+// reduce verbosity of returning type and heap error address
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 // default all var and funcs are private
 // - using 'pub' here to grant main.rs visibility
 pub fn run(config: Config) -> MyResult<()> {
-    println!("Lib.rs:: Hello World!");
+    for filename in &config.files{
+        // borrowing &filename
+        match open(&filename) {
+            // good form to eprint error to stderr
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                // _ place holder, currently we aren't printing line numbers
+                //for (_, line) in file.lines().enumerate() {
+                for line in file.lines(){
+                    println!("{}", line?);
+                }
+            }
+        }
+    }
     dbg!(config);
     Ok(())
 }
@@ -33,24 +39,62 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("MTON <mton@aol.com>")
         .about("Rust cat")
+        // positional arguments
         .arg(
             Arg::with_name("files")
                 .value_name("FILE")
-                .help("Input file(s) [default: -]")
+                .help("Input file(s)")
+                // @audit : explain these entries
+                .multiple(true)
+                .default_value("-")
         )
+        // optional arguments
+        // - can have short and long names
         .arg(
             Arg::with_name("line_numbers")
                 .short("n")
                 .long("number")
                 .help("Number lines")
+                // this is a flag and does NOT take a value
+                .takes_value(false) 
+                // can not occur in conjunction with -b
+                // - could this have been specified in the -b match instead?
+                .conflicts_with("line_numbers_non_blank")
         )
         .arg(
             Arg::with_name("line_numbers_non_blank")
                 .short("b")
                 .long("number-nonblank")
                 .help("Number lines (includes non blanks)")                
+                // this is a flag and does NOT take a value
+                .takes_value(false)
         )
         .get_matches();
     
-    Ok(Config::default())
+    Ok(Config{
+        // because there's a default value, it should be save to call unwrap()
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("line_numbers"),
+        number_nonblank_lines: matches.is_present("line_numbers_non_blank")
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        // when filename is "-"
+        // - read from stdin()
+        "-" => Ok(
+                    Box::new(
+                        BufReader::new(io::stdin())
+                    )
+                ),
+        // else try to open given filename
+        _ => Ok(
+                Box::new(
+                    BufReader::new(
+                        File::open(filename)?
+                    )
+                )
+            ),
+    }
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     // try to execute lib.rs run function
     // @audit : explain modules ... is src/lib.rs an implicit constant?
-    if let Err(e) = catr::run() {
+    // ANSWER : lib/run() is accessed through `catr` the project crate
+    if let Err(e) = catr::get_args().and_then(catr::run) {
         // use error print line to print error message to STDERR
         eprintln!("{}", e);
         // Exit program with a nonzero value to indicate an error
