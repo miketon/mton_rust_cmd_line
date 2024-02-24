0744d3643c54de435b1180548159b61dee126609
Mike Ton <mike.ton@gmail.com>
2024-02-24 05:58:49 -0600

    updating cli.rs to test file piping with run_stdin : TEST PASSED

--- headr/tests/cli.rs
+++ headr/tests/cli.rs
@@ -1,6 +1,6 @@
 use assert_cmd::Command;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
 use predicates::prelude::*;
 use rand::{Rng, distributions::Alphanumeric};
@@ -40,6 +40,30 @@ fn run(args: &[&str], expected_file: &str) -> TestResult {
         .success()
         .stdout(predicate::eq(&expected.as_bytes() as &[u8]));
 
+    Ok(())
+}
+
+fn run_stdin(
+    args: &[&str],
+    input_file: &str,
+    expected_file: &str,
+) -> TestResult {
+    // Extra work here due to lossy UTF
+    let mut file = File::open(expected_file)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    let expected = String::from_utf8_lossy(&buffer);
+    
+    let input = fs::read_to_string(input_file)?;
+   
+    Command::cargo_bin(PRG)?
+        // @audit : Explain this difference vs run()
+        .write_stdin(input)
+        .args(args)
+        .assert()
+        .success()
+        .stdout(predicate::eq(&expected.as_bytes() as &[u8]));
+    
     Ok(())
 }
 
@@ -110,8 +134,18 @@ fn one() -> TestResult {
 }
 
 #[test]
+fn one_stdin() -> TestResult {
+    run_stdin(&[], ONE, "tests/expected/one.txt.out")
+}
+
+#[test]
 fn two() -> TestResult {
     run(&[TWO], "tests/expected/two.txt.out")
+}
+
+#[test]
+fn two_stdin() -> TestResult {
+    run_stdin(&[], TWO, "tests/expected/two.txt.out")
 }
 
 #[test]
@@ -120,8 +154,18 @@ fn three() -> TestResult {
 }
 
 #[test]
+fn three_stdin() -> TestResult {
+    run_stdin(&[], THREE, "tests/expected/three.txt.out")
+}
+
+#[test]
 fn ten() -> TestResult {
     run(&[TEN], "tests/expected/ten.txt.out")
+}
+
+#[test]
+fn ten_stdin() -> TestResult {
+    run_stdin(&[], TEN, "tests/expected/ten.txt.out")
 }
 
 //---------------------------------------------------------------------------80
@@ -284,4 +328,3 @@ fn multipile_files_c2() -> TestResult {
 fn multiple_files_c4() -> TestResult {
     run(&[EMPTY, ONE, TWO, THREE, TEN, "-c", "4"], "tests/expected/all.c4.out")
 }
-
